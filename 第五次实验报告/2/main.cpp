dd21e2cd8bcfc57c0e75c244c265a82841104e8a
Elstarry <2940210094@qq.com>
2024-12-16 20:18:41 +0800

    Update main.cpp

--- 第五次实验报告/2/main.cpp
+++ 第五次实验报告/2/main.cpp
@@ -1,11 +1,11 @@
-//main.cpp                      主函数模块
-#include <iostream>             //将类声明头文件包含进来
+//main.cpp                    
+#include <iostream>            
 #include "student.h"
 int main()
 {
-	Student stud;                //定义对象
+	Student stud;             
 	Student stud1(007, "tcg", 'm');
-	stud.display();              //执行stud对象的display函数
-	stud1.display();			//执行stud1对象的display函数
+	stud.display();          
+	stud1.display();		
 	return 0;
 }
